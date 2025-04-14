f77c0fc38cfd6da9378ee87a3266701e87a1f4ab
rgsubramaniam <83640961+rgsubramaniam@users.noreply.github.com>
2025-04-14 16:26:29 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
 provider "aws" {
-  region  = "us-west-2"
-  access_key = var.access
-  secret_key = var.secret
+  region  = "ap-south-1"
 }
 
 resource "aws_instance" "app_server" {
   ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
 }
 
