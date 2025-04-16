5af5f13025554ea12b167b01f0fd8cb49d06d514
rgsubramaniam <83640961+rgsubramaniam@users.noreply.github.com>
2025-04-16 21:43:46 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,1 +1,11 @@
+output "instance_public_ip" {
+  value = aws_instance.web.public_ip
+}
 
+output "instance_private_ip" {
+  value = aws_instance.web.private_ip
+}
+
+output "instance_hostname" {
+  value = aws_instance.web.private_dns
+}
